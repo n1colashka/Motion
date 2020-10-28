6804bd06d944ef8fa8319cb0ee1dbbd8ef3b298c
Nick <deckards2505@gmail.com>
2020-10-28 16:05:17 +0600

    feat: add main page

--- app/js/main.js
+++ app/js/main.js
@@ -5,17 +5,33 @@ document.addEventListener('DOMContentLoaded', function() {
         const menuWrapper = document.querySelector('.profile-menu');
 
         document.body.addEventListener('click', event => {
-            if (event.target.closest('.profile-menu') || event.target.closest('.header__btn')) {
+            if (event.target.closest('.profile-menu')) {
+                menuWrapper.classList.add('active');
+                menuBtn.classList.add('active');
+            } else if (event.target.closest('.header__btn')) {
                 menuWrapper.classList.toggle('active');
                 menuBtn.classList.toggle('active');
-            } else {
+            } 
+            else {
                 menuWrapper.classList.remove('active');
                 menuBtn.classList.remove('active');
             }
         });
     }
 
+    function changePageType() {
+        const types = document.querySelectorAll('.header__links a');
+        const activeType = document.querySelector('.header__links .active');
+
+        activeType.addEventListener('click', function() {
+            types.forEach(type => {
+                type.style.display = "inline-block";
+            });
+        });
+    }
+
 
     initMenu();
+    changePageType();
     
 });
--- dist/js/main.js
+++ dist/js/main.js
@@ -5,7 +5,10 @@ document.addEventListener('DOMContentLoaded', function () {
     var menuBtn = document.querySelector('.header__btn');
     var menuWrapper = document.querySelector('.profile-menu');
     document.body.addEventListener('click', function (event) {
-      if (event.target.closest('.profile-menu') || event.target.closest('.header__btn')) {
+      if (event.target.closest('.profile-menu')) {
+        menuWrapper.classList.add('active');
+        menuBtn.classList.add('active');
+      } else if (event.target.closest('.header__btn')) {
         menuWrapper.classList.toggle('active');
         menuBtn.classList.toggle('active');
       } else {
@@ -15,5 +18,16 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   }
 
+  function changePageType() {
+    var types = document.querySelectorAll('.header__links a');
+    var activeType = document.querySelector('.header__links .active');
+    activeType.addEventListener('click', function () {
+      types.forEach(function (type) {
+        type.style.display = "inline-block";
+      });
+    });
+  }
+
   initMenu();
+  changePageType();
 });
